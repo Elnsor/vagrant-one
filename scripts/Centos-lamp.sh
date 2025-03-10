a9808dc9f04ee1aeec7306e4ea26853a70446b6d
Elnsor <elkasseh633@gmail.com>
2025-03-10 03:03:35 +0200

    update some file

--- scripts/Centos-lamp.sh
+++ scripts/Centos-lamp.sh
@@ -14,3 +14,8 @@ rm -rf /var/www/html
 
 ## make symbolic link to document apache server
 ln -s vagrant /var/www/html
+
+## get index from repo 
+sudo -u vagrant wget -q https://raw.githubusercontent.com/Elnsor/vagrant-one/refs/heads/main/files/index.html
+systemctl start httpd
+
